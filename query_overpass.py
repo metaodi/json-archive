fcecfa8b4307c5d8abc54ac17aebacaca31e1e4b
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-11-24 15:06:12 +0100

    Read from stdin and write to stdout

--- query_overpass.py
+++ query_overpass.py
@@ -1,24 +1,29 @@
-import overpass
-from pprint import pprint
-import json
-
-# use the Swiss Overpass
-api = overpass.API(endpoint="http://overpass.osm.ch/api/interpreter")
-
-# Adapted Overpass Query from https://github.com/Schutz-Rettung-Zurich/json-archive
-defi_zurich = """
-area[name="Zürich"]["wikipedia"="de:Zürich"]->.zurich;
-// gather results
-(
-  node["emergency"="defibrillator"](area.zurich);
-  way["emergency"="defibrillator"](area.zurich);
-  relation["emergency"="defibrillator"](area.zurich);
-);
-// print results
-out body;
->;
-out skel qt;
+# -*- coding: utf-8 -*-
 """
-results = api.get(defi_zurich)
-with open('data/defis_stadt_zh.geojson', 'w') as t:
-    t.write(json.dumps(results, sort_keys=True, indent=2))
+Query OpenStreetMap data from OverPass API
+This script reads a query from stdin and writes the resulting GeoJSON to stdout
+
+Example:
+    cat queries/defis_stadt_zh.txt | python query_overpass.py > data/defis_stadt_zh.geojson
+"""
+
+import os
+import sys
+import traceback
+import json
+import overpass
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
+try:
+    API_ENDPOINT = os.getenv('OVERPASS_API_ENDPOINT', 'http://overpass.osm.ch/api/interpreter')
+    api = overpass.API(endpoint=API_ENDPOINT)
+
+    query = "".join(sys.stdin.readlines())
+    results = api.get(query)
+    print(json.dumps(results, sort_keys=True, indent=2))
+except Exception as e:
+    print("Error: %s" % e, file=sys.stderr)
+    print(traceback.format_exc(), file=sys.stderr)
+    sys.exit(1)
